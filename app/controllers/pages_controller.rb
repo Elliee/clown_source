3c91916ab66d5e135193da6433c3dc8b69eb1b71
Ellie Stevens <elliebstevens@gmail.com>
2021-12-02 18:17:37 +1100

    added comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,9 @@ class PagesController < ApplicationController
 
   def dashboard
     #find the logged in user
+    #get all users clown listings
     #display the users clown listings => user.clowns
+    #get all users bookings
     #display all the users bookings => user.bookings
 
   end
