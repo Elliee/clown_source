51e8deb694cbb274e04520f362c91444f5f9575a
Ellie Stevens <elliebstevens@gmail.com>
2021-12-02 19:28:12 +1100

    added model validations for clown and booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :users
   belongs_to :clowns
+  validates :start_date, :end_date, presence: true
 end
--- app/models/clown.rb
+++ app/models/clown.rb
@@ -1,3 +1,5 @@
 class Clown < ApplicationRecord
   belongs_to :user
+
+  validates :name, :description, :price, presence: true
 end
